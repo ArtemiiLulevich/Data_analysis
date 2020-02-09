0920953581fee78601e63285dd18faff8fac1345
Artemii <artemii.lulevich@gmail.com>
2020-02-09 13:05:46 +0200

    Add color map and first and last points of the walk.

--- Examples/rw_visual.py
+++ Examples/rw_visual.py
@@ -6,7 +6,14 @@ while True:
     rw = RandomWalk()
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = list(range(rw.num_points))
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
+                cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    plt.scatter(0, 0, c='green', s=30)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=30)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
