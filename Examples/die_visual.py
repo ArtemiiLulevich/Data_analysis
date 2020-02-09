e9f404a656fdad3dc614768aa0ce7e3a317e900d
Artemii <artemii.lulevich@gmail.com>
2020-02-09 16:16:52 +0200

    Refactoring.

--- Examples/die_visual.py
+++ Examples/die_visual.py
@@ -13,7 +13,7 @@ for roll_num in range(1000):
 
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(1, max_result + 1):
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
