57f8b9fe59fc6fe70d2f51bc391b9cf5940889d8
Artemii <artemii.lulevich@gmail.com>
2020-02-18 20:33:44 +0200

    highs_lows.py add ylim..

--- Data loading/highs_lows.py
+++ Data loading/highs_lows.py
@@ -19,17 +19,17 @@ with open(filename) as f:
         low = int(row[3])
         lows.append((low - 32) / 1.8)
 
-    fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(dates, highs, c='red', alpha=0.5)
-    plt.plot(dates, lows, c='blue', alpha=0.5)
-    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+plt.ylim(0, 50)
 
-    plt.title("Daily high temperatures - 2014", fontsize=24)
-    plt.xlabel('', fontsize=16)
+plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
 
-    fig.autofmt_xdate()
-    plt.ylabel("Temperature (C)", fontsize=16)
-    plt.tick_params(axis='both', which='major', labelsize=16)
-
-    plt.show()
+fig.autofmt_xdate()
+plt.ylabel("Temperature (C)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
 
